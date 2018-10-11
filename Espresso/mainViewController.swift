713233e1a09ad0623fff1d92fa3a7921bca9dcb8
Ryan Guarascia <rguarascia@gmail.com>
2018-10-10 23:35:00 -0400

    Added log in functionality.

--- Espresso/mainViewController.swift
+++ Espresso/mainViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class mainViewController: UIViewController {
-
+class mainViewController: UIViewController, dataProtocol {
+    var feedItems: NSArray = NSArray();
+    var selectedRow : dataModel = dataModel()
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    func itemsDownloaded(items: NSArray) {
+        feedItems = items;
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        errorLabel.text = "";
+        for item in feedItems {
+            let x: dataModel = item as! dataModel
+            
+            if(username.text != "" || password.text != "") {
+                if(x.username! == username.text! && x.password! == password.text!) {
+                    errorLabel.text = "Log in successful";
+                    break;
+                } else {
+                    errorLabel.text = "Username and or password incorrect";
+                }
+            } else {
+                errorLabel.text = "Username or password empty";
+            }
+        }
+    }
+    
     
     @IBAction func gotoCreateAccount(_ sender: Any) {
         performSegue(withIdentifier: "gotoCreate", sender: self);
@@ -17,6 +45,10 @@ class mainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad();
+        
+        let DataHelper = dataHelper();
+        DataHelper.delegate = self;
+        DataHelper.downloadItems();
     }
     
     @IBAction func gotoView(_ sender: Any) {
